4e1bf9f8a7423dd8fbfd7bbb75902beb9652d87f
Devesh Pal <newdev0@outlook.com>
2024-10-18 20:16:47 +0530

    Update

--- tgplugins/admin_control.py
+++ tgplugins/admin_control.py
@@ -500,12 +500,6 @@ async def imdb_callback(bot: Client, quer_y: CallbackQuery):
         )
 
 
-@Client.on_message(filters.command("logs") & filters.user(ADMINS))
-async def log_file(bot, message):
-    try:
-        await message.reply_document("BotLog.txt")
-    except Exception as e:
-        await message.reply(str(e))
 
 
 @Client.on_message(filters.command("restart") & filters.user(ADMINS))
